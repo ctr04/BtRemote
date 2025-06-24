195eb817f2bf3e2591eacc8e60cb3bdd8cdedda8
Atharok <atharok@duck.com>
2025-06-24 10:01:54 +0200

    New release (1.7.2).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.atharok.btremote"
         minSdk = 28
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.7.1"
+        versionCode = 15
+        versionName = "1.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
